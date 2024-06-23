bb7310852d060f56899ba30a9a525476b229428b
Nikita Belkin <belkinsrc@gmail.com>
2024-06-23 09:22:34 +0300

    config: make vite-template-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import TemplatePlugin from './plugins/vite-template-plugin';
 
 export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  plugins: [TemplatePlugin()],
 });
