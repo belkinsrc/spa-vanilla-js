a17e409301a617b05e6a82fcd2e80ef5c79c3c48
Nikita Belkin <belkinsrc@gmail.com>
2024-06-28 08:42:32 +0300

    feat: create API Client for posts

---
+++ src/api/postsApi.js
@@ -0,0 +1,19 @@
+import baseApi from './baseApi';
+
+export const getPosts = (page) => {
+  return baseApi.get(`/posts?_page=${page}&_limit=10&_expand=user`);
+};
+
+export const getPostsByUser = (userId, page) => {
+  return baseApi.get(
+    `/posts?userId=${userId}&_page=${page}&_limit=10&_expand=user`
+  );
+};
+
+export const getPostById = (postId) => {
+  return baseApi.get(`/posts?postId=${postId}&_expand=user`);
+};
+
+export const getPostsSearch = (search, page) => {
+  return baseApi.get(`/posts?q=${search}&_page=${page}&_limit=10&_expand=user`);
+};
