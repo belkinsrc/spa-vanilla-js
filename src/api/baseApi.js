fb22be82dd4ebe10d99d7ef2bd57e72b4f9688cf
Nikita Belkin <belkinsrc@gmail.com>
2024-06-28 08:15:02 +0300

    feat: add a second level of abstraction for the rest api

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -1,6 +1,6 @@
 const apiServer = import.meta.env.DEV ?? 'http://localhost:1111';
 
-const baseFetch = (url, config = {}, params) => {
+const baseFetch = (url, params, config = {}) => {
   return new Promise((resolve, reject) => {
     try {
       const _config = {
@@ -19,4 +19,44 @@ const baseFetch = (url, config = {}, params) => {
       reject(error);
     }
   });
+};
+
+const fetchGet = (url, params = {}) => {
+  return baseFetch(url, params);
+};
+
+const fetchPost = (url, params = {}, config = {}) => {
+  return baseFetch(url, params, {
+    ...config,
+    method: 'POST',
+  });
+};
+
+const fetchPut = (url, params = {}, config = {}) => {
+  return baseFetch(url, params, {
+    ...config,
+    method: 'PUT',
+  });
+};
+
+const fetchPatch = (url, params = {}, config = {}) => {
+  return baseFetch(url, params, {
+    ...config,
+    method: 'PATCH',
+  });
+};
+
+const fetchDelete = (url, params = {}, config = {}) => {
+  return baseFetch(url, params, {
+    ...config,
+    method: 'DELETE',
+  });
+};
+
+export default {
+  get: fetchGet,
+  post: fetchPost,
+  put: fetchPut,
+  patch: fetchPatch,
+  delete: fetchDelete,
 };
