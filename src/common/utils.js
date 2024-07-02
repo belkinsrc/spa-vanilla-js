46bbf990d12ce0ca506f0a9dd8ba9938e504d20b
Nikita Belkin <belkinsrc@gmail.com>
2024-07-02 11:01:00 +0300

    feat: add util function 'colorForString'

--- src/common/utils.js
+++ src/common/utils.js
@@ -27,6 +27,25 @@ export const randomColor = () => {
   return `#${padZero(Math.floor(Math.random() * 16777215).toString(16), 6)}`;
 };
 
+export const colorForString = (str, seed = 0) => {
+  //it uses cyrb53
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 =
+    Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^
+    Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 =
+    Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^
+    Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  const hash = `${4294967296 * (2097151 & h2) + (h1 >>> 0)}`;
+  return `#${hash.substring(1, 7)}`;
+};
+
 export const getUserInitials = (userName) => {
   if (userName) {
     const data = userName.split('.');
@@ -59,8 +78,10 @@ export const highlightText = (text = '', search = '') => {
 };
 
 export default {
-  randomColor,
   invertColor,
+  randomColor,
+  colorForString,
   getUserInitials,
+  dateFormat,
   highlightText,
 };
