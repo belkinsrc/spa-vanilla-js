59432efd2c28d308a897cd2542a73f4f6db03aec
Nikita Belkin <belkinsrc@gmail.com>
2024-07-28 14:23:08 +0500

    feat: add DB connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+const URL = 'mongodb://localhost:27017/BlogPlatformDB';
 const PORT = 5000;
+
+mongoose
+  .connect(URL)
+  .then(() => console.log('Connected to DB'))
+  .catch((err) => console.log(`DB connection error: ${err}`));
 
 const app = express();
 
@@ -8,6 +15,6 @@ app.use((req, res) => {
   res.send('Server listening...');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server was launched on port ${PORT}`);
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`Server was launched on port ${PORT}`);
 });
