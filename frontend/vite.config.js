a7d4bddb8f572c42e4b97a4e1e3742750fb9d4d1
Nikita Belkin <belkinsrc@gmail.com>
2024-09-23 20:49:51 +0300

    configure proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import TemplatePlugin from './plugins/vite-template-plugin';
+import { defineConfig } from 'vite'
+import path from 'path'
+import TemplatePlugin from './plugins/vite-template-plugin'
 
 export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   plugins: [TemplatePlugin()],
-});
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
